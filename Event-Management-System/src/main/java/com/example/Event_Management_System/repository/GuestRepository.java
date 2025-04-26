1cd18840110c12ab9be3dba1d8a6b5326fee782c
Wania-n <wanianaeem48@gmail.com>
2025-04-26 21:24:53 +0500

    Just checking

--- Event-Management-System/src/main/java/com/example/Event_Management_System/repository/GuestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Event_Management_System.repository;
-
-import com.example.Event_Management_System.model.Guest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GuestRepository extends JpaRepository<Guest, Integer> {
-    // Add all the custome queries here!
-}
--- Event-Management-System/src/main/java/com/example/Event_Management_System/repository/VendorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Event_Management_System.repository;
-
-import com.example.Event_Management_System.model.Vendor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VendorRepository extends JpaRepository<Vendor, Integer> {
-    // Add all the custome queries here!
-}
